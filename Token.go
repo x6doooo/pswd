1b57eaf7ca4960500496758cd2cf438d189c52a6
x6doooo <x6doooo@gmail.com>
2016-09-29 11:24:15 +0800

    update

--- Token.go
+++ Token.go
@@ -38,8 +38,8 @@ func(me *Token) Decrypt(content []byte) []byte {
     decrypter := cipher.NewCFBDecrypter(me.keyBlock, me.iv)
     decrypted := make([]byte, len(content))
     decrypter.XORKeyStream(decrypted, content)
-    if strings.Contains(string(decrypted), me.salt) {
-        str := strings.Replace(string(decrypted), me.salt, "", 1)
+    if strings.Contains(string(decrypted), string(me.salt)) {
+        str := strings.Replace(string(decrypted), string(me.salt), "", 1)
         return []byte(str)
     }
     return []byte("")
